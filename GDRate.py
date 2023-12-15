8584aa6d03c461ae527e48aeddadb2a603a66e3b
M336_ <75208577+M336G@users.noreply.github.com>
2023-12-15 23:38:04 +0100

    v1.0.2

--- GDRate.py
+++ GDRate.py
@@ -1,11 +1,18 @@
 import requests
 import base64
+import os
 from itertools import cycle
 
 def xor_cipher(input_string, key):
     return ''.join(chr(ord(c)^ord(k)) for c,k in zip(input_string, cycle(key)))
 
 def main():
+    global databaseUrl
+    global accessSecret
+    global modSecret
+    global accountID
+    global encoded_base64
+
     databaseUrl = "http://www.boomlings.com/database/"
 
     xorKey = "37526"
@@ -19,7 +26,6 @@ def main():
     if databaseUrlRequest != "":
         databaseUrl = databaseUrlRequest
 
-
     print(f"Database URL: {databaseUrl}\n")
 
     accountID = input("Account ID: ")
@@ -30,6 +36,9 @@ def main():
     encoded_base64 = encoded_base64.replace("+", "-")
     encoded_base64 = encoded_base64.replace("/", "_")
     try:
+        headers = {
+            "User-Agent": ""
+        }
         data = {
             "accountID": accountID,
             "gjp": encoded_base64,
@@ -38,73 +47,106 @@ def main():
             "binaryVersion": 35,
             "gdw": 0
         }
-
-        req = requests.post(f'{databaseUrl}requestUserAccess.php', data=data)
-
+        req = requests.post(f'{databaseUrl}requestUserAccess.php', data=data, headers=headers)
+        os.system('cls')
         if req.text == "-1":
             print("You are not a Moderator!")
-            return 0
+            main()
         
         elif req.text == "1":
             print("You are now logged in as a Moderator")
+            rateOrDemonChoice()
         
         elif req.text == "2":
             print("You are now logged in as an Elder Moderator")
-        
-        rateOrDemon = input("Do yo want to suggest stars for a level (0) or rate a demon level difficulty (1): ")
-        if rateOrDemon == "0":
-            levelID = input("Level ID: ")
-            stars = input("Stars: ")
-            feature = input("Feature (0 for no, 1 for yes; default is 0): ")
-            data = {
-                "gameVersion": 21,
-                "binaryVersion": 35,
-                "accountID": accountID,
-                "gjp": encoded_base64,
-                "levelID": levelID,
-                "stars": stars,
-                "feature": feature,
-                "gdw": 0,
-                "secret": modSecret
-            }
-
-            req = requests.post(f"{databaseUrl}suggestGJStars20.php", data=data)
-            print(req.text)
-            if req.text == "1":
-                print("Success!")
-                return 0
-            else: 
-                print("Something went wrong...")
-                return 0
-
-        elif rateOrDemon == "1":
-            levelID = input("Level ID: ")
-            rating = input("Demon Difficulty (1 for Easy Demon, 2 for Medium Demon, 3 for Hard Demon, 4 for Insane Demon and 5 for Extreme Demon): ")
-            headers = {
-            }
-
-            data = {
-                "gameVersion": 21,
-                "binaryVersion": 35,
-                "accountID": accountID,
-                "gjp": encoded_base64,
-                "secret": modSecret,
-                "levelID": levelID,
-                "rating": rating
-            }
-
-            req = requests.post(f'{databaseUrl}rateGJDemon21.php', headers=headers, data=data)
-
-            if req.text == levelID:
-                print("Success!")
-                return 0
-            else:
-                print("Something went wrong...")
-                return 0
+            rateOrDemonChoice()
         else:
-            print("Wrong option")
-            return 0
+            print("Wrong Account ID/password")
+
     except:
         print("An error occured while trying to connect to the database")
-        return 0
+        main()
+        
+def rateOrDemonChoice():
+    os.system('cls')
+    print("\nPress ENTER to go back\n")
+    rateOrDemon = input("Do yo want to suggest stars for a level (0) or rate a demon level difficulty (1): ")
+    if rateOrDemon == "0":
+        suggestStars()
+    elif rateOrDemon == "1":
+        rateDemon()
+    elif rateOrDemon == "":
+        main()
+    else:
+        print("Wrong option")
+        rateOrDemonChoice()
+        
+def suggestStars():
+    os.system('cls')
+    print("\nPress ENTER to go back\n")
+    levelID = input("Level ID: ")
+    if levelID == "":
+        rateOrDemonChoice()
+    stars = input("Stars: ")
+    if stars == "":
+        rateOrDemonChoice()
+    feature = input("Feature (0 for no, 1 for yes; default is 0): ")
+    if feature == "":
+        feature = 0
+
+    headers = {
+        "User-Agent": ""
+    }
+    data = {
+        "gameVersion": 21,
+        "binaryVersion": 35,
+        "accountID": accountID,
+        "gjp": encoded_base64,
+        "levelID": levelID,
+        "stars": stars,
+        "feature": feature,
+        "gdw": 0,
+        "secret": modSecret
+    }
+
+    req = requests.post(f"{databaseUrl}suggestGJStars20.php", data=data, headers=headers)
+    if req.text == "1":
+        print("Success!")
+        rateOrDemonChoice()
+    else: 
+        print("Something went wrong...")
+        suggestStars()
+
+def rateDemon():
+    os.system('cls')
+    print("\nPress ENTER to go back\n")
+    levelID = input("Level ID: ")
+    if levelID == "":
+        rateOrDemonChoice()
+    rating = input("Demon Difficulty (1 for Easy Demon, 2 for Medium Demon, 3 for Hard Demon, 4 for Insane Demon and 5 for Extreme Demon): ")
+    if rating == "":
+        rateOrDemonChoice()
+    headers = {
+        "User-Agent": ""
+    }
+
+    data = {
+        "gameVersion": 21,
+        "binaryVersion": 35,
+        "accountID": accountID,
+        "gjp": encoded_base64,
+        "secret": modSecret,
+        "levelID": levelID,
+        "rating": rating
+    }
+
+    req = requests.post(f'{databaseUrl}rateGJDemon21.php', data=data, headers=headers)
+
+    if req.text == levelID:
+        print("Success!")
+        rateOrDemonChoice()
+    else:
+        print("Something went wrong...")
+        rateDemon()
+
 main()
